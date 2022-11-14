1570317182ffc88628e0e1683d2cb9babb3a447e
shilatorkaby <shillat14@gmail.com>
2022-11-14 13:59:07 +0200

    add Connect singleton

--- src/main/java/Client/Client.java
+++ src/main/java/Client/Client.java
@@ -7,17 +7,10 @@ import java.util.List;
 public class Client {
     public static void main(String[] args) {
         Repository<User> repository = new Repository<>(User.class);
-
         List<User> users = repository.ConnectionToSQL(User.class);
         users.forEach(user -> System.out.println(user.toString()));
-
-        repository.addObjectToDB(User.class,new User());
- //       Repository.insertUser();
-//        Repository.deleteUser();
-//        Repository.updateUser();
-
-//        List<User> users = Repository.ConnectionToSQL();
-//        users.forEach(user -> System.out.println(users.toString()));
+        System.out.println("get user by id:\n"+repository.getItemById(User.class,2));
+        System.out.println("get user by property:\n"+repository.getItemByProperty(User.class,"password",1234));
 
 
     }
