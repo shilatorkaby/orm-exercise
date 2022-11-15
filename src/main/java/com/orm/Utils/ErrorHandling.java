898d3aef92ce0d523bc8f0c27974f6ed242be000
shilatorkaby <shillat14@gmail.com>
2022-11-15 13:18:31 +0200

    add tests to ErrorHandeling class

--- src/main/java/com/orm/Utils/ErrorHandling.java
+++ src/main/java/com/orm/Utils/ErrorHandling.java
@@ -41,14 +41,14 @@ public class ErrorHandling {
         validateItem(item, logger);
     }
 
-    private static void validateId(int id, Logger logger) {
+    static void validateId(int id, Logger logger) {
         if (id <= 0) {
             logger.error("id should not be 0 or minus");
             throw new IllegalArgumentException("id should not be 0 or minus");
         }
     }
 
-    private static <T> void validateClz(Class<T> clz, Logger logger) {
+    static <T> void validateClz(Class<T> clz, Logger logger) {
         if (clz == null) {
             logger.error("class should not be null");
             throw new IllegalArgumentException("class should not be null");
@@ -56,14 +56,14 @@ public class ErrorHandling {
 
     }
 
-    private static <T> void validateItem(T item, Logger logger) {
+    static <T> void validateItem(T item, Logger logger) {
         if (item == null) {
             logger.error("item should not be null");
             throw new IllegalArgumentException("item should not be null");
         }
     }
 
-    private static void validatePropertyName(String propertyName, Logger logger) {
+    static void validatePropertyName(String propertyName, Logger logger) {
         if (propertyName == null || propertyName.equals("")) {
             logger.error("property name should not be null or empty");
             throw new IllegalArgumentException("property name should not be null or empty");
